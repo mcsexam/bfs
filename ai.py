c97223dfbc0a2409eb535447c5526b0ffd2798b8
mcsexam <rohanjadhavyt07@gmail.com>
2025-10-08 10:46:47 +0530

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque()
+
+    visited.add(start)
+    queue.append(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=' ')
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+def main():
+    start_node = 'A'
+    print("Breadth-First Search Traversal:")
+    bfs(graph, start_node)
+    print() 
+
+if __name__ == '__main__':
+    main()
